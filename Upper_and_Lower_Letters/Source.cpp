957e9f81d246952e4b06bc75b17617f3c8d81613
Phurich63010767 <69179855+Phurich63010767@users.noreply.github.com>
2020-09-16 14:28:07 +0700

    Update Source.cpp

--- Upper_and_Lower_Letters/Source.cpp
+++ Upper_and_Lower_Letters/Source.cpp
@@ -5,14 +5,17 @@ void change(int i, int s, string x)
 {
     while (i < s)
     {
+        int t = 0;
         char* p;
-        p = &x[i];
+        p = &x[i];              
         if (i % 2 == 0)
         {
             if (*p >= 97 && *p <= 122)
             {
                 *p = *p - 32;
             }
+            if (*p >= 65 && *p <= 90)
+            cout << *p;
         }
         if (i % 2 != 0)
         {
@@ -20,18 +23,33 @@ void change(int i, int s, string x)
             {
                 *p = *p + 32;
             }
-        }
-        cout << *p;
-        i++;
-    }
+            if (*p >= 97 && *p <= 122)
+            cout << *p;
+        }        
+        i++;        
+    }    
 }
 int main()
-{
-    int i = 0, s;
+{    
+    START:    
+    int i = 0, s, t = 0;
     string x;
-    cin >> x;
+    cout << "Enter letters: ";
+    cin >> x;   
     s = x.size();
+    while (t < s)
+    {
+        char* p1 = &x[t];
+        if (*p1 < 65 || (*p1 > 90 && *p1 < 97) || *p1 > 122)
+        {
+            cout << "Enter letter only." << endl;
+            system("pause");
+            system("CLS");
+            goto START;
+        }
+        t++;
+    }
     change(i, s, x);
-
+   
     return 0;
 }
